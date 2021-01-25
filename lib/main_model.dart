53a402debd56725cb9cf434cadbbb7510f28daff
shusuke-463 <shusuke463@gmail.com>
2021-01-25 19:07:46 +0900

    second commit

--- lib/main_model.dart
+++ lib/main_model.dart
@@ -6,10 +6,22 @@ class MainModel extends ChangeNotifier {
   List<Todo> todoList = [];
 
   Future getTodoList() async {
-    final snapshot = await FirebaseFirestore.instance.collection('todoList').get();
+    final snapshot = await FirebaseFirestore.instance.collection('todoList')
+        .get();
     final docs = snapshot.docs;
     final todoList = docs.map((doc) => Todo(doc)).toList();
     this.todoList = todoList; //上にアクセス
     notifyListeners(); //←代入したことを伝える
   }
+    void getTodoListRealtime() {
+      final snapshots = FirebaseFirestore.instance.collection('todoList')
+          .snapshots();
+      snapshots.listen((snapshot) {
+        final docs = snapshot.docs;
+        final todoList = docs.map((doc) => Todo(doc)).toList();
+        todoList.sort((a,b) => a.createdAt.compareTo(b.createdAt)); //ソートの記述
+        this.todoList = todoList;
+        notifyListeners();
+      });
+    }
 }
